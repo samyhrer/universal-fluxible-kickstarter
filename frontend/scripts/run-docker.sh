3b1c45073514f475657119ad810cdf03a379b01c
Svein Arild Myhrer <svein.arild.myrer@gmail.com>
2016-08-23 14:11:19 +0000

    working

--- frontend/scripts/run-docker.sh
+++ frontend/scripts/run-docker.sh
@@ -2,6 +2,7 @@ docker stop frontend
 docker rm frontend
 docker build -f ./Dockerfile-dev -t frontend . &&
 docker run \
+        -p 3000:3000 \
         -v $PWD/src:/usr/app/src/src \
         -v $PWD/scripts:/usr/app/src/scripts \
         -v $PWD/config:/usr/app/src/config \
